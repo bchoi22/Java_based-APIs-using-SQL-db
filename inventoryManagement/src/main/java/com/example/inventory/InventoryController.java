cc6cf24d2eac17f271e9d11943987acc139bd775
Dan <Dan@Dan-PC>
2019-02-12 21:11:24 -0600

    Changed to a REST app. This will now work as a micro service/api

--- inventoryManagement/src/main/java/com/example/inventory/InventoryController.java
+++ inventoryManagement/src/main/java/com/example/inventory/InventoryController.java
@@ -1,29 +1,52 @@
 package com.example.inventory;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Map;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class InventoryController {
 	
+	@Autowired
+	InventoryManagementApplication inventoryManagement;
 	
-	/*
-	 * This is our login page 
-	 * and main enterence into the app.
+	
+	/*login method is set up to take a json 
+	 * payload and return a string as a response. 
+	 * This is just for testing. Eventually it will return
+	 * a json object containing all of the data needed to 
+	 * build the users dashboard if they successfully authenticate in.
+	 * 
+	 * test with: curl -H "Content-Type: application/json" --data '{"username":"xyz","password":"xyz"}' @body.json http://localhost:8080/login
 	 */
-	@RequestMapping("/")
-	public String index(Model model) {
-		return "index";
+	@RequestMapping(
+			value = "/login",
+			method = RequestMethod.POST)
+	@ResponseBody
+	public String login(@RequestBody Map<String, Object> payload) {
+		//The controller receives the request from the front end and then sends it
+		//to inventoryManagement to perform processing. 
+		String response = inventoryManagement.authenticateIntoApplication(payload); 
+		return response;
 	}
 	
 	
-	/*
-	 * Once authenticated they will
-	 * be forwarded to the dashboard.
-	 */
-	@RequestMapping("/dashboard")
-    public String dashboard(Model model) {
+	@RequestMapping("/createDashboard")
+	@ResponseBody
+    public String createDashboard() {
+        return "dashboard";
+    }
+	
+	
+	
+	@RequestMapping("/saveDashboard")
+	@ResponseBody
+    public String saveDashboard() {
         return "dashboard";
     }
 }
--- inventoryManagement/src/main/java/com/example/inventory/InventoryManagementApplication.java
+++ inventoryManagement/src/main/java/com/example/inventory/InventoryManagementApplication.java
@@ -1,5 +1,7 @@
 package com.example.inventory;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,14 @@ public class InventoryManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementApplication.class, args);
 	}
+
+	public String authenticateIntoApplication(Map<String, Object> payload) {
+		// TODO Auto-generated method stub
+		//Perform logic to authenticate into the application
+		return "You're In!!";
+	}
+
+
 
 }
 
