de9357810a94d5851316977f291397967b0d3b2f
Dan <Dan@Dan-PC>
2019-03-18 19:28:26 -0500

    return dashboard

---
+++ inventoryManagement/src/main/java/com/example/inventory/Items.java
@@ -0,0 +1,46 @@
+package com.example.inventory;
+
+public class Items {
+	private String partNo;
+	private String serialNo;
+	private int weight;
+	
+	public Items(String partNo, String serialNo, int weight) {
+		super();
+		this.partNo = partNo;
+		this.serialNo = serialNo;
+		this.weight = weight;
+	}
+	
+	public Items() {
+		
+	}
+	
+	public String getPartNo() {
+		return partNo;
+	}
+
+	public void setPartNo(String partNo) {
+		this.partNo = partNo;
+	}
+
+	public String getSerialNo() {
+		return serialNo;
+	}
+
+	public void setSerialNo(String serialNo) {
+		this.serialNo = serialNo;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	
+}
+
+    
---
+++ inventoryManagement/src/main/java/com/example/inventory/Unit.java
@@ -0,0 +1,32 @@
+package com.example.inventory;
+
+import java.util.List;
+
+public class Unit {
+	private boolean hasWeight;
+	private List<Items> items;
+	
+	public Unit(boolean hasWeight, List<Items> items) {
+		super();
+		this.hasWeight = hasWeight;
+		this.items = items;
+	}
+
+	public boolean isHasWeight() {
+		return hasWeight;
+	}
+
+	public void setHasWeight(boolean hasWeight) {
+		this.hasWeight = hasWeight;
+	}
+
+	public List<Items> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Items> items) {
+		this.items = items;
+	}
+	
+	
+}
